19e642d682e83176b92019e0f4c8499ba349b1d3
toryoto <ryoto.taga12@gmail.com>
2024-09-28 03:21:22 +0900

    トランザクションデータをサーバで保存する

--- blockchain.py
+++ blockchain.py
@@ -1,41 +1,55 @@
 from ecdsa import VerifyingKey, BadSignatureError, SECP256k1
 import binascii
 import json
+import pandas as pd
+import os
+
+TRANSACTIO_FILE = "./transaction_data.pkl"
 
 class BlockChain(object):
-  def __init__(self):
-    self.transaction_pool = { "transactions": [] }
-  
-  def add_transaction_pool(self, transaction):
-    # 送信するトランザクションがすでに存在しない場合は追加
-    if transaction not in self.transaction_pool["transaction"]:
-      self.transaction_pool["transactions"].append(transaction);
-      return True
-    else:
-      return False
-  
-  def verify_transaction(self, transaction):
-    # 送金額がマイナスはFalse
-    if transaction["amount"] < 0:
-      return False
-    
-    # 送信者の公開鍵をバイナリに
-    public_key =  VerifyingKey.from_string(binascii.unhexlify(transaction["sender"]), curve=SECP256k1)
-		
+	def __init__(self):
+		self.transaction_pool = {"transactions": []}
+
+	def save_transaction_pool(self):
+		pd.to_pickle(self.transaction_pool, TRANSACTIO_FILE)
+
+	def load_transaction_pool(self):
+		if os.path.isfile(TRANSACTIO_FILE):
+			transaction_data = pd.read_pickle(TRANSACTIO_FILE)
+			return transaction_data
+		else:
+			return {"transaction": []}
+
+	def add_transaction_pool(self, transaction):
+		# 送信するトランザクションがすでに存在しない場合は追加
+		if transaction not in self.transaction_pool["transaction"]:
+			self.transaction_pool["transactions"].append(transaction)
+			return True
+		else:
+			return False
+
+	def verify_transaction(self, transaction):
+		# 送金額がマイナスはFalse
+		if transaction["amount"] < 0:
+			return False
+
+		# 送信者の公開鍵をバイナリに
+		public_key = VerifyingKey.from_string(binascii.unhexlify(transaction["sender"]), curve=SECP256k1)
+
 		# トランザクションの署名を取得
-    signature = binascii.unhexlify(transaction["signature"])
-		
+		signature = binascii.unhexlify(transaction["signature"])
+
 		# 署名以外のトランザクション
-    unsigned_transactions = { 
-			"time": transaction["time"], 
-			"sender": transaction["sender"], 
-			"receiver": transaction["receiver"], 
-			"amount": transaction["amount"] 
+		unsigned_transactions = {
+			"time": transaction["time"],
+			"sender": transaction["sender"],
+			"receiver": transaction["receiver"],
+			"amount": transaction["amount"]
 		}
-		
-    try:
+
+		try:
 			# 送信者の署名を、送信者の公開鍵を使用して検証
-      flg = public_key.verify(signature, json.dumps(unsigned_transactions).encode('utf-8'))
-      return flg
-    except BadSignatureError:
-      return False
+			flg = public_key.verify(signature, json.dumps(unsigned_transactions).encode('utf-8'))
+			return flg
+		except BadSignatureError:
+			return False
--- main.py
+++ main.py
@@ -10,20 +10,22 @@ class Transaction(BaseModel):
   amount: int
   signature: str
 
-transaction_pool = { "transactions": [] }
+blockchain = blockchain.BlockChain()
+blockchain.transaction_pool = blockchain.load_transaction_pool()
+
 app = FastAPI()
 
 @app.get("/transaction_pool")
 def get_transaction_pool():
-  return transaction_pool
+  return blockchain.transaction_pool
 
 @app.post("/transaction_pool")
 def post_transaction(transaction :Transaction):
-  print(transaction)
   transation_dict = transaction.dict()
   if blockchain.verify_transaction(transation_dict):
     if blockchain.add_transaction_pool(transation_dict):
-    	return { "message": "Transaction is posted." }
+      blockchain.save_transaction_pool()
+      return { "message": "Transaction is posted." }
   
   
 if __name__ == "__main__":
